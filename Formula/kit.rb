85a960e670ec0daaf50110d893021d53f307f61a
zaidhassan168 <zaidhassan168@outlook.com>
2026-04-22 10:09:57 +0500

    Bump kit to v1.0.4 (harden stale JSON handling)

--- Formula/kit.rb
+++ Formula/kit.rb
@@ -1,8 +1,8 @@
 class Kit < Formula
   desc "Kubernetes Interactive Toolkit for Azure Kubernetes Service (AKS)"
   homepage "https://github.com/zaidhassan168/aks-tools"
-  url "https://github.com/zaidhassan168/aks-tools/archive/refs/tags/v1.0.3.tar.gz"
-  sha256 "79c115b05a15eaa642985807f7989ca50d51d25b6ac7f2236099f3d543c0db43"
+  url "https://github.com/zaidhassan168/aks-tools/archive/refs/tags/v1.0.4.tar.gz"
+  sha256 "3df821eda01f9e2a831c065a1fd4458004354eb8c52a3dd073bfcc954149380a"
   license "MIT"
 
   depends_on "azure-cli"
